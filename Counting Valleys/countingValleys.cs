2d12c2005b26ac1d3a0150602f482b9f4208878c
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-12-07 21:48:19 +0300

    hackerrank counting valleys algoritm solved

---
+++ Counting Valleys/countingValleys.cs
@@ -0,0 +1,65 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        int statue = 0;
+        int valleyCount = 0;
+        for(int i = 0; i< steps; i++){
+            
+            if(path[i].Equals('U')){
+                statue++;
+                if(statue == 0){
+                    valleyCount++;
+                }
+            }
+            else if(path[i].Equals('D')){
+                statue--;
+            }
+        }
+        return valleyCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
