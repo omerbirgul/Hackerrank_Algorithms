bed85eecf4a5575109b4f4f3442397b16dd6ef43
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-10-12 18:54:24 +0300

    migratoryBirds algoritm solved

--- README.md
+++ README.md
@@ -7,4 +7,8 @@
 | 0005 | [Simple Array Sum](https://www.hackerrank.com/challenges/simple-array-sum/problem) | [simpleArraySum.cs](./algorithms/05_Simple_Array_Sum/simpleArraySum.cs) |
 | 0006 | [Very Big Sum](https://www.hackerrank.com/challenges/a-very-big-sum/problem) | [veryBigSum.cs](./algorithms/06_Very_Big_Sum/aVeryBigSum.cs) |
 | 0007 | [Mini Max Sum](https://www.hackerrank.com/challenges/mini-max-sum/problem) | [miniMaxSum.cs](./algorithms/07_Mini_Max_Sum/miniMaxSum.cs) |
+| 0008 | [Diagonal Difference](https://www.hackerrank.com/challenges/diagonal-difference/problem) | [diagonalDifference.cs](./algorithms/08_Diagonal_Difference/diagonalDifference.cs) |
+| 0009 | [Birthday Cake Candles](https://www.hackerrank.com/challenges/birthday-cake-candles/problem) | [birthdayCakeCandles.cs](./algorithms/09_Birthday_Cake_Candles/birthdayCakeCandles.cs) |
+| 0010 | [Grading Students](https://www.hackerrank.com/challenges/grading/problem) | [gradingStudents.cs](./algorithms/10_Grading_Students/gradingStudents.cs) |
 | 0033 | [Subarray Division](https://www.hackerrank.com/challenges/the-birthday-bar/problem) | [subarrayDivision.cs](./algorithms/33_Subarray_Division/subarrayDivision.cs) |
+| 0033 | [Migratory Birds](https://www.hackerrank.com/challenges/migratory-birds/problem) | [migratoryBirds.cs](./algorithms/34_Migratory_Birds/migratoryBirds.cs) |
---
+++ algorithms/34_Migratory_Birds/migratoryBirds.cs
@@ -0,0 +1,62 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<int> arr)
+    {
+        Dictionary<int, int> birdsIdAndCount = new Dictionary<int, int>();
+        foreach(var item in arr)
+        {
+            if(!birdsIdAndCount.ContainsKey(item))
+            {
+                birdsIdAndCount[item] = 1;
+            }else{
+                birdsIdAndCount[item]++;
+            }
+        }
+        int maxValue = birdsIdAndCount
+            .Where(x => x.Value == birdsIdAndCount.Values.Max())
+            .OrderBy(x => x.Key)
+            .FirstOrDefault().Key;
+        return maxValue;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.migratoryBirds(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
